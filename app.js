376038abd6f0c954c56d17876ba2c98fdfce58f9
Abel George <abel.paul.george@gmail.com>
2018-10-29 16:32:45 -0400

    added more get requests

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ var app = express();
 app.get("/",function(req,res){
     res.send("Hi There")
 })
+
+app.get("/bye",function(req,res){
+    res.send("Adios")
+})
+
+app.get("/dog",function(req,res){
+    res.send("MEOW")
+})
