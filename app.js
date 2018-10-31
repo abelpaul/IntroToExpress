4bc1fd21b3930a26a1caeb541ba3471f5229ba3f
Abel George <abel.paul.george@gmail.com>
2018-10-31 18:13:43 -0400

    fixed wildcard routing

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ var app = express();
 
 app.get("/",function(req,res){
     res.send("Hi There")
-})
+});
 
 app.get("/bye",function(req,res){
     res.send("Adios")
-})
+});
 
 app.get("/dog",function(req,res){
     res.send("MEOW")
+});
+
+app.get("*",function(req,res){
+    res.send("not here homie")
 })
 
 app.listen(3000, function(){
-    console.log("Hi from Port 3000")
+    console.log("Hi from Port 3000");
 })
